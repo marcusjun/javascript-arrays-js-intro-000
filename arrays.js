28dc93503ee3c85d36aebe9b4234131243694260
Marcus Jun <mjun@calstate.edu>
2018-03-04 00:30:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"]
 
 function addElementToBeginningOfArray(array,element){
-  array=[element,...array]
+  var newArray=[element,...array]
+  return newArray
 }
